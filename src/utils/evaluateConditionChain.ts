7ceebde5709def18b600f5bb19dedbacebc64eeb
Abderrazzaq Khastaf <abderrazzaqkhastaf@gmail.com>
2023-08-20 18:31:00 +0100

    implement the function evaluateConditionChain

--- src/utils/evaluateConditionChain.ts
+++ src/utils/evaluateConditionChain.ts
@@ -5,9 +5,43 @@ export const evaluateConditionChain = (
   conditionChain: ConditionChain,
   object: Record<string, unknown>,
 ): boolean => {
-  // TODO: implement
+  if (conditionChain.operator === 'and')
+    return conditionChain.conditions.every((condition: Condition | ConditionChain) => evaluateCondition(condition, object))
+  else if (conditionChain.operator === 'or')
+    return conditionChain.conditions.some((condition: Condition | ConditionChain) => evaluateCondition(condition, object))
   return false;
 };
+
+function evaluateCondition(condition: Condition | ConditionChain, object: Record<string, unknown>) : boolean {
+  if ("conditions" in condition && "operator" in condition)
+    return evaluateConditionChain(condition, object)
+  const value: any = get(object, condition.key)
+  switch (condition.operator) {
+    case "equal":
+      return value === condition.value
+    case "notEqual":
+      return value !== condition.value
+    case "greaterThan":
+      return value > condition.value
+    case "greaterThanOrEqual":
+      return value >= condition.value
+    case "lessThan":
+      return value < condition.value
+    case "lessThanOrEqual":
+      return value <= condition.value
+    case "contains":
+      if (typeof value === 'string' || Array.isArray(value))
+        return value.includes(condition.value)
+      return false
+    case "notContains":
+      if (typeof value === 'string' || Array.isArray(value))
+        return !value.includes(condition.value)
+      return false
+    default:
+      return false
+  }
+}
+
 
 export interface ConditionChain {
   conditions: Array<Condition | ConditionChain>;
